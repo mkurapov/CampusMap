af1c2545724bfdb8a478b5991195a9624f62e73f
Max Kurapov <maxkurapov@gmail.com>
2019-03-02 22:21:09 -0700

    trying with calgary geojson

--- main.js
+++ main.js
@@ -1,65 +1,52 @@
-const buildingArray = [
-    'AA','AB','AU','BS','CC','CCIT','CD','CH','CHCP','CR','DC','ED','EEEL','EN','ES','GL','ICT','IH','KA','KNA','KNB','MFH','MH','MLB','MLT','MS','MSC','OL','OO','PF','RC','RT','RU','SA','SB','SH','SS','ST','TFDL','TI','YA' ];
+const w = 1728;
+const h = 1152;
 
-const svg = d3.select("svg");
-d3.csv('data/paths-2013.csv').then(pathData => { 
-    displayBuildings();
-    
+const svg = d3.select(".map")
+.append("svg")
+.attr("height", h)
+.attr("width", w);
 
-    const pathIds = pathData.map(row => row.Path_ID);
-    pathIds.forEach(id => displayPath(id, pathData.filter(row => row.Path_ID == id)));
+var projection = d3.geoAzimuthalEqualArea()
+  .scale(9999999)
+  .center([-114.134352, 51.078010])
+  .translate([480, 250]);
+
+var geoGenerator = d3.geoPath()
+  .projection(projection);
+
+const path = d3.geoPath()
+.projection(projection);
+
+let tooltip = d3.select("body")
+.append("div")
+.attr("class", "tooltip");
+
+const files = ["data/calgary.geojson"];
+
+Promise.all(files.map((url) => d3.json(url))).then(function(data) {
+
+  d3.csv("data/paths-test.csv").then(pathData => {
+      svg.append("g")
+        .attr("class", "country")
+        .selectAll("path")
+        .data(data[0].features)
+        .enter().append("path")
+        .attr("d", path);
+         
+    svg.selectAll(".meteor")
+      .data(pathData)
+      .enter().append("circle")
+      .attr("class", "meteor")
+      .attr("cx", (d,i) => {
+          console.log([d.Lat, d.Lon]);
+          let coords = projection([+d.Lon, +d.Lat]);
+          console.log(coords);
+        return coords[0];
+      })
+      .attr("cy", (d) => {
+        let coords = projection([+d.Lon, +d.Lat]);
+        return coords[1];
+      })
+      .attr("r", 6);
+  });
 });
-
-function displayBuildings() {
-    const buildings = svg.selectAll('g')
-    .data(buildingArray);
-
-    buildings
-        .merge(buildings)
-        .attr('class', 'building')
-        .attr('id', d => d)
-        .on('mouseover', function() {
-            // console.log(d3.select(this).attr('id'))
-        });
-}
-
-const generatePath = d3.line()
-    .x(function(d) { return latToX(d.Lat); })
-    .y(function(d) { return longToY(d.Lon); });
-
-function displayPath(pathId, pathData) {   
-    const path = svg.append("path")
-    .datum(pathData)
-    .attr("class", "line") 
-        .attr("stroke", getRandomColor()) 
-        .attr("d", generatePath)
-
-    const totalLength = path.node().getTotalLength();
-
-    path
-    .attr("stroke-dasharray", totalLength + " " + totalLength)
-    .attr("stroke-dashoffset", totalLength)
-    .transition()
-        .duration(3000)
-        // .ease("linear")
-        .attr("stroke-dashoffset", 0);
-}
-
-function latToX(lat) {
-    lat = (+lat).toFixed(5);
-    const magicNumber = 6844;
-    const sub = lat.substring(4, lat.length);
-    return sub - magicNumber;
-}
-
-
-function longToY(long) {
-    long = (+long).toFixed(4);
-    const magicNumber = 1029;
-    const sub = long.substring(5, long.length);
-    return sub - magicNumber;
-}
-
-function getRandomColor() {
-    return "#"+((1<<24)*Math.random()|0).toString(16)
-  }
