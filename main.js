f609285ee43836ea751888d37ef1e0bcac6cb194
Max Kurapov <maxkurapov@gmail.com>
2019-03-02 23:39:45 -0700

    nearly there

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const w = 1728;
-const h = 1152;
+const width = 1728;
+const height = 1152;
 
-const svg = d3.select(".map")
-.append("svg")
-.attr("height", h)
-.attr("width", w);
+const svg = d3.select("svg");
 
-var projection = d3.geoAzimuthalEqualArea()
-  .scale(9999999)
-  .center([-114.134352, 51.078010])
-  .translate([480, 250]);
+function getZoom() {
+    return 150;
+}
 
-var geoGenerator = d3.geoPath()
-  .projection(projection);
+let zoomscale = 150;
+
+var projection = d3.geoMercator()
+  .scale(5800000)
+  .center([-114.130583, 51.077945])
+//   .translate([w / 2, h / 2])
+  .translate([890, 540]);
+
+//   51.077886, -114.130563
 
 const path = d3.geoPath()
 .projection(projection);
@@ -21,32 +24,40 @@ let tooltip = d3.select("body")
 .append("div")
 .attr("class", "tooltip");
 
-const files = ["data/calgary.geojson"];
-
-Promise.all(files.map((url) => d3.json(url))).then(function(data) {
-
-  d3.csv("data/paths-test.csv").then(pathData => {
-      svg.append("g")
-        .attr("class", "country")
-        .selectAll("path")
-        .data(data[0].features)
-        .enter().append("path")
-        .attr("d", path);
-         
-    svg.selectAll(".meteor")
-      .data(pathData)
-      .enter().append("circle")
-      .attr("class", "meteor")
-      .attr("cx", (d,i) => {
-          console.log([d.Lat, d.Lon]);
-          let coords = projection([+d.Lon, +d.Lat]);
-          console.log(coords);
-        return coords[0];
-      })
-      .attr("cy", (d) => {
+d3.csv("data/paths-test2.csv").then(pathData => {
+//   svg.append("g")
+//     .attr("class", "country")
+//     .selectAll("path")
+//     .data(data[0].features)
+//     .enter().append("path")
+//     .attr("d", path);
+        const center = {
+            Lon:-114.13563,
+            Lat: 51.077886
+        }
+svg.selectAll(".meteor")
+    .data(pathData)
+    .enter().append("circle")
+    .attr("class", "meteor")
+    .attr("cx", (d,i) => {
+        // console.log([d.Lat, d.Lon]);
         let coords = projection([+d.Lon, +d.Lat]);
-        return coords[1];
-      })
-      .attr("r", 6);
-  });
+        // console.log(coords);
+    return coords[0];
+    })
+    .attr("cy", (d) => {
+    let coords = projection([+d.Lon, +d.Lat]);
+    return coords[1];
+    })
+    .attr("r", 10);
 });
+var zoom = d3.zoom().on("zoom", zoomed);
+
+
+svg
+    .call(zoom);
+
+
+function zoomed() {
+    zoomscale = d3.event.transform.k * 1000;
+  }
