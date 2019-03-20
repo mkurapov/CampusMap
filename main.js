170b23f9c6a3b37e686dd1b43cff898d63b72b65
Max Kurapov <maxkurapov@gmail.com>
2019-03-20 15:13:42 -0600

    drawing paths

--- main.js
+++ main.js
@@ -2,60 +2,117 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWt1cmFwb3YiLCJhIjoiY2p0aGo5NW0yMGZnaDN5cGY4N
 
 const map = new mapboxgl.Map({
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-114.130099, 51.077311],
-    zoom: 15.5,
+    center: [-114.1313808,  51.0774501],
+    zoom: 14.5,
     // pitch: 45,
     // bearing: -17.6,
     container: 'map'
 });
 
-let speedFactor = 30; // number of frames per longitude degree
+let speedFactor = 3000; // number of frames per longitude degree
 let animation; // to store and cancel the animation
 let startTime = 0;
 let progress = 0; // progress = timestamp - startTime
-
+let geojson;
+let timePerPath = 10;
+let paths;
+var resetTime = false; // indicator of whether time reset is needed for the animation
+var pauseButton = document.getElementById('pause');
+let currentPathIndex = 0;
+let currentCoordIndex = 0;
 
 function animateLine(timestamp) {
-    if (resetTime) {
-    // resume previous progress
-        startTime = performance.now() - progress;
-        resetTime = false;
+    progress = timestamp - startTime;
+
+    let isDrawing = false;
+    if (progress < timePerPath) {
+       
     } else {
-        progress = timestamp - startTime;
+        startTime = performance.now();
+        isDrawing = true;
+    }
+
+    if (isDrawing) {
+         console.log('drawing');
+        if (currentPathIndex < paths.length - 1) {
+            currentPathIndex++;
+            geojson.features.push(paths[currentPathIndex]);
+            console.log(paths[currentPathIndex]);
+            map.getSource('paths_layer').setData(geojson);
+        } else {
+            geojson.features = [];
+            currentPathIndex = 0;
+        }
+
+        isDrawing = false;
     }
     
-    // restart if it finishes a loop
-    if (progress > speedFactor * 360) {
-        startTime = timestamp;
-        geojson.features[0].geometry.coordinates = [];
-    } else {
-        const x = progress / speedFactor;
-    // append new coordinates to the lineString
-        geojson.features[0].geometry.coordinates.push([x, y]);
-    // then update the map
-        map.getSource('line-animation').setData(geojson);
-    }
+    // // // restart if it finishes a loop
+    // if (progress > speedFactor * 1) {
+    //     // console.log('restart');
+    //     startTime = timestamp;
+    //     // geojson.features[0].geometry.coordinates = [];
+    // } else {
+    //         // console.log('now');
+    //         
+    //     }
+        
+    // // append new coordinates to the lineString
+        
+    // // then update the map
+    //     
+    // }
     // Request the next frame of the animation.
-        animation = requestAnimationFrame(animateLine);
+    animation = requestAnimationFrame(animateLine);
 }
 
 
 map.on('load', () => {
-    map.addSource('paths', {
-        type: 'geojson',
-        data: '/data/geopaths-small.json'
-    });
 
-    map.addLayer({
+    d3.json('/data/pathsgeo.json').then(data => {
+        geojson = data;
+        // geojson.features.splice(1);
+        paths = geojson.features;
+        // map.addSource('paths', {
+        //     type: 'geojson',
+        //     data: geojson
+        // });
+
+        map.addLayer({
             "id": "paths_layer",
             "type": "line",
-            "source": "paths",
+            "source": {
+                "type": "geojson",
+                "data": geojson
+            },
             'paint': {
                 'line-color': '#ed6498',
                 'line-width': 1,
                 'line-opacity': 0.2
             }
-    });
+        });
+            
+
+        
+        // pauseButton.addEventListener('click', () => {
+        //     pauseButton.classList.toggle('pause');
+        //     if (pauseButton.classList.contains('pause')) {
+        //         cancelAnimationFrame(animation);
+        //     } else {
+        //         resetTime = true;
+        //         animateLine();
+        //     }
+        //     });
+
+        startTime = performance.now();
+        geojson.features = [];
+        map.getSource('paths_layer').setData(geojson);
+ 
+        animateLine();
+
+        animation = requestAnimationFrame(animateLine);
+    })
+    
 });
 
 
